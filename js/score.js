1d6b73bce6d806a6a65f6bd256afbc79337fd97a
Shadz <141278526+shadzhog@users.noreply.github.com>
2025-01-20 17:52:13 +1300

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-1*Math.pow(rank-1, 2) + 500) *
+    let score = ((-1/rank-1,) + 500/1.035) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
