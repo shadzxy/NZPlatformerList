fb6e292d8f00564b25577f0f3dd1199e91a72c1e
Shadz <141278526+shadzxy@users.noreply.github.com>
2025-02-17 08:52:24 +1300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -51,7 +51,7 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
-                            <div class="type-title-sm">Password</div>
+                            <div class="type-title-sm">First Victor Time</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
                     </ul>
